37f5d1460a723e9b8e9f84bc836aa79e6c4129bd
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-05-23 02:05:17 +0000

    Changes to be committed: 	modified:   src/components/Charts/SparkLine.jsx 	modified:   src/pages/ECommerce.jsx

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-export default function SparkLine() {
+export default function SparkLine({
+  currentColor,
+  id,
+  height,
+  width,
+  type,
+  color,
+  data,
+}) {
   return (
-    <div>SparkLine</div>
-  )
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType={"Numeric"}
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 }
