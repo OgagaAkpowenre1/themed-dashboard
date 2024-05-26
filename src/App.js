8d00f62db9c1ceb28cce1b3d5e71922cee8f568b
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-05-26 16:44:51 +0000

    Changes to be committed: 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/pages/ColorPicker.jsx 	modified:   src/pages/Editor.jsx 	modified:   src/pages/Kanban.jsx

--- src/App.js
+++ src/App.js
@@ -59,6 +59,8 @@ function App() {
             </div>
 
             <div>
+              <ThemeSettings />
+
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<ECommerce />} />
