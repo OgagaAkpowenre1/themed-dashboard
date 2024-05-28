ef829cf339df59bf4ef6c12be0911f32cf97dfaa
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-05-28 18:03:40 +0000

    Changes to be committed: 	modified:   src/App.css 	modified:   src/components/Charts/LineChart.jsx 	modified:   src/components/Charts/Pie.jsx 	modified:   src/components/Charts/SparkLine.jsx 	modified:   src/pages/Charts/Area.jsx 	modified:   src/pages/Charts/Bar.jsx 	modified:   src/pages/Charts/ColorMapping.jsx 	modified:   src/pages/Charts/Financial.jsx 	modified:   src/pages/Charts/Line.jsx 	modified:   src/pages/Charts/Pie.jsx 	modified:   src/pages/Charts/Pyramid.jsx 	modified:   src/pages/Charts/Stacked.jsx

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  SplineAreaSeries,
+  DateTime,
+  Legend,
+} from "@syncfusion/ej2-react-charts";
+import { Header } from "../../components";
+import {
+  areaCustomSeries,
+  areaPrimaryXAxis,
+  areaPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 export default function Area() {
+  const { currentMode } = useStateContext();
   return (
-    <div>Area</div>
-  )
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category={"Area"} title={"Inflation Rate In Percentage"} />
+      <ChartComponent
+        id="line-chart"
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      >
+        <Inject services={[DateTime, Legend, SplineAreaSeries]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 }
